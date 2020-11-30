69897a0944c9c6981b9170b30c6740265dfae261
renehernandez <renehr9102@protonmail.com>
2020-11-30 06:51:25 -0500

    Initial commit

---
+++ Formula/appfile.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Appfile < Formula
+    desc "Deploy App specs to DigitalOcean App Platform"
+    homepage "https://github.com/renehernandez/appfile"
+    url "https://github.com/renehernandez/appfile/archive/v0.0.3.tar.gz"
+    sha256 "ca7d55c417c888b4b4aa324c300919434c4ee15d4c8ab8a623fc239b8a873f41"
+    license "MIT"
+
+    depends_on "go" => :build
+
+    def install
+        system "go", "build", "-ldflags", "-X github.com/renehernandez/appfile/internal/version.Version=#{version}",
+        "-o", bin/appfile, "-v", "github.com/renehernandez/appfile"
+    end
+
+    test do
+        assert_match version.to_s, shell_output("#{bin}/appfile -v")
+    end
+  end
---
+++ README.md
@@ -0,0 +1,11 @@
+# My Taps
+
+## How do I install these formulae?
+
+`brew install renehernandez/taps/<formula>`
+
+Or `brew tap renehernandez/taps` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
