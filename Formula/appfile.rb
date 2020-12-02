ea2a308d4bc8c114cdebd5662606b201b607a529
Rene Hernandez <7692501+renehernandez@users.noreply.github.com>
2020-12-02 15:21:01 -0500

    Update appfile to v0.0.4

--- Formula/appfile.rb
+++ Formula/appfile.rb
@@ -1,7 +1,7 @@
 class Appfile < Formula
     desc "Deploy App specs to DigitalOcean App Platform"
     homepage "https://github.com/renehernandez/appfile"
-    url "https://github.com/renehernandez/appfile.git", tag: "v0.0.4", revision: "26449dd4df1c24ae9ac5a21c9b239ad9da2fe719"
+    url "https://github.com/renehernandez/appfile.git", tag: "v0.0.4", revision: "521ecb4e1bceef5368035c9c4be03970795c5ac3"
     license "MIT"
 
     depends_on "go" => :build
