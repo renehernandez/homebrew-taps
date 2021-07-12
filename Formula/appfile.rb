9030a717a881ba26752b245e64ebbe89f0a33e93
Rene Hernandez <7692501+renehernandez@users.noreply.github.com>
2021-07-12 08:31:54 -0400

    Add bottles for appfile v0.0.9 on macOS 10.15 (Catalina)

--- Formula/appfile.rb
+++ Formula/appfile.rb
@@ -4,6 +4,12 @@ class Appfile < Formula
   url "https://github.com/renehernandez/appfile.git", tag: "v0.0.9", revision: "3a51d7641820aeacadd4521b4a7ceb5c1f64df1f"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/renehernandez/appfile/releases/download/v0.0.9"
+
+    sha256 cellar: :any, catalina: "4d1b8af022c552cfc036a37c7bc883aa3dc8a5c4be7c6ee78c01baf055e17e42"
+  end
+
   depends_on "go" => :build
 
   def install
